5c8db2bb7a19d3c731bd0818a1843307196f69bd
mvandervoord <mvandervoord@e7d17a6e-8845-0410-bbbc-c8efb4fdad7e>
2009-02-22 00:04:53 +0000

    git-svn-id: http://unity.svn.sourceforge.net/svnroot/unity/trunk@20 e7d17a6e-8845-0410-bbbc-c8efb4fdad7e

--- src/UnityHelper.h
+++ src/UnityHelper.h
@@ -9,7 +9,7 @@ void AssertEqualCharArray(const char expected[], const char actual[], const int 
 void AssertFloatArrayWithin(const float tolerance, const float* expected, const float* actual, const unsigned int length);
 void AssertEqualExampleStruct(const EXAMPLE_STRUCT_T expected, const EXAMPLE_STRUCT_T actual);
 
-#define TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, length) {AssertEqualUintArray(expected, actual, length);}
+#define TEST_ASSERT_EQUAL_uint32_ARRAY(expected, actual, length) {AssertEqualUintArray(expected, actual, length);}
 #define TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, length) {AssertEqualIntArray(expected, actual, length);}
 #define TEST_ASSERT_FLOAT_ARRAY_WITHIN(tolerance, expected, actual, length) {AssertFloatArrayWithin(tolerance, expected, actual, length);}
 #define TEST_ASSERT_EQUAL_EXAMPLE_STRUCT_T(expected, actual) {AssertEqualExampleStruct(expected, actual);}
