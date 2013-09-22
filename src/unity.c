ff1339ca5ab96512d1c068693b3caab7604eaee4
Mark VanderVoord <mvandervoord@gmail.com>
2013-09-22 10:58:36 -0400

    - Fixed a gcc warning when -Wall enabled.  (Thanks gjcamann)

--- src/unity.c
+++ src/unity.c
@@ -14,7 +14,7 @@
 #define UNITY_SKIP_EXECUTION  { if ((Unity.CurrentTestFailed != 0) || (Unity.CurrentTestIgnored != 0)) {return;} }
 #define UNITY_PRINT_EOL       { UNITY_OUTPUT_CHAR('\n'); }
 
-struct _Unity Unity = { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , { 0 } };
+struct _Unity Unity = { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , {{{ 0 }}} };
 
 const char* UnityStrNull     = "NULL";
 const char* UnityStrSpacer   = ". ";
