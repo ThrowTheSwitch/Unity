9842a4d03ddb3a2f3ef48e247a470e99921eb134
Mark VanderVoord <mvandervoord@gmail.com>
2019-10-21 10:12:59 -0400

    Switching to universal version being in the header file itself.

--- src/unity.h
+++ src/unity.h
@@ -8,6 +8,11 @@
 #define UNITY_FRAMEWORK_H
 #define UNITY
 
+#define UNITY_VERSION_MAJOR    2
+#define UNITY_VERSION_MINOR    5
+#define UNITY_VERSION_BUILD    0
+#define UNITY_VERSION          ((UNITY_VERSION_MAJOR << 16) | (UNITY_VERSION_MINOR << 8) | UNITY_VERSION_BUILD)
+
 #ifdef __cplusplus
 extern "C"
 {
