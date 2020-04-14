a2af08c773803755e90614615fe9b649f5b242e6
Vitalii Shylienkov <vshilenkov@luxoft.com>
2020-04-14 11:02:24 +0200

    project: revert UNITY_VERSION_* to unity.h

--- src/unity.h
+++ src/unity.h
@@ -8,7 +8,10 @@
 #define UNITY_FRAMEWORK_H
 #define UNITY
 
-#include "unity_version.h"
+#define UNITY_VERSION_MAJOR    2
+#define UNITY_VERSION_MINOR    5
+#define UNITY_VERSION_BUILD    0
+#define UNITY_VERSION          ((UNITY_VERSION_MAJOR << 16) | (UNITY_VERSION_MINOR << 8) | UNITY_VERSION_BUILD)
 
 #ifdef __cplusplus
 extern "C"
