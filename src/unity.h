cf949f45ca6d172a177b00da21310607b97bc7a7
Mark VanderVoord <mvandervoord@gmail.com>
2020-05-03 16:03:07 -0400

    Bump Version

--- src/unity.h
+++ src/unity.h
@@ -10,7 +10,7 @@
 
 #define UNITY_VERSION_MAJOR    2
 #define UNITY_VERSION_MINOR    5
-#define UNITY_VERSION_BUILD    0
+#define UNITY_VERSION_BUILD    1
 #define UNITY_VERSION          ((UNITY_VERSION_MAJOR << 16) | (UNITY_VERSION_MINOR << 8) | UNITY_VERSION_BUILD)
 
 #ifdef __cplusplus
