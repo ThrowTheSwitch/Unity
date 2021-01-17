ffb51ecb7e30c62a8439ef893fb40ed63d51b664
Mark VanderVoord <mvandervoord@gmail.com>
2021-01-16 20:05:20 -0500

    Switch from Travis to Github Actions. Update year in docs.

--- src/unity.c
+++ src/unity.c
@@ -1,6 +1,6 @@
 /* =========================================================================
     Unity Project - A Test Framework for C
-    Copyright (c) 2007-19 Mike Karlesky, Mark VanderVoord, Greg Williams
+    Copyright (c) 2007-21 Mike Karlesky, Mark VanderVoord, Greg Williams
     [Released under MIT License. Please refer to license.txt for details]
 ============================================================================ */
 
